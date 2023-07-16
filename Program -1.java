365d9622e54b7b80829df21afa67a97e6e16f8e5
Rakshithacnaik <139698780+Rakshithacnaik@users.noreply.github.com>
2023-07-16 20:35:19 +0530

    Update Program -1.java

--- Program -1.java
+++ Program -1.java
@@ -1,1 +1,39 @@
+import java.util.Scanner;
 
+class Calculator {
+    private double a;
+    private double b;
+    private String operationType;
+
+    public Calculator(double a, double b, String operationType) {
+        this.a = a;
+        this.b = b;
+        this.operationType = operationType;
+    }
+
+    public double calculate() {
+        double result = 0.0;
+        switch (operationType) {
+            case "add":
+                result = a + b;
+                break;
+            case "subtract":
+                result = a - b;
+                break;
+            case "multiply":
+                result = a * b;
+                break;
+            case "divide":
+                if (b != 0) {
+                    result = a / b;
+                } else {
+                    System.out.println("Error: Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operation type.");
+                break;
+        }
+        return result;
+    }
+}
